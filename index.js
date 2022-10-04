c634200d39030795a76fac704fc556e75eaef999
Mas-Tool <miky@mas-tool.com>
2022-10-03 20:42:43 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
-import Arlo from './lib/arlo.mjs';
+'use strict';
 
-export default Arlo;
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var _arlo = require('./lib/arlo.mjs');
+
+var _arlo2 = _interopRequireDefault(_arlo);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+exports.default = _arlo2.default;
